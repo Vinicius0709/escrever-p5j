cb5dddc2492f3f6d54cbf5b91800b0688092849a
Vinicius0709 <viniciusvasconcel2@gmail.com>
2024-05-23 09:08:05 -0300

    coloquei tudo

--- sketch.js
+++ sketch.js
@@ -1,16 +1,39 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
 function setup() {
-    createCanvas(400 , 400);
-      background("blue");
+  createCanvas(400, 400);
+  background(color(100,0,0));
+  cor=color(random(0,255),random(0,255),random(0,255));
+  //(red, gree, blue)
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+function draw() {
+ 
+  fill(cor)
+  circle(posicaoHorizontal,posicaoVertical,50);
+  
+ 
+  if (mouseX < posicaoHorizontal){
+      posicaoHorizontal = posicaoHorizontal - 1;
+  }
+   
+  if (mouseX > posicaoHorizontal){
+     posicaoHorizontal = posicaoHorizontal + 1;
+  }
+ 
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
   }
   
-  function draw() {
-    stroke("black");
-     fill("red");
-  
-  // console.log(mouseIsPressed);
-    
-    if(mouseIsPressed) {
-         rect(mouseX, mouseY, 30, 30);
-    }
-    
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
   }
+  
+  if (mouseIsPressed){ cor=color(random(0,255),random(0,255),random(0,255),random(0,100));
+  }
+
+}
